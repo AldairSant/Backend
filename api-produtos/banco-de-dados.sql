cd64955a553cf4d24c4dd4bb821a14e533236bd4
AldairSant <167237501+AldairSant@users.noreply.github.com>
2024-08-12 00:30:45 -0300

    Update banco-de-dados.sql

--- api-produtos/banco-de-dados.sql
+++ api-produtos/banco-de-dados.sql
@@ -4,8 +4,8 @@ USE catalogo_produtos;
 
 CREATE TABLE produtos
 (
-    id (INT, AUTO_INCREMENT, PRIMARY KEY)
-    nome (VARCHAR(255))
-    descricao (TEXT)
-    preco (DECIMAL(10, 2))
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255),
+    descricao TEXT,
+    preco DECIMAL(10, 2)
 );
